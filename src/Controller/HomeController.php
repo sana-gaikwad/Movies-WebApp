e3733380799e9a1dcf91d02deb6bfc7bb9b5a57c
SanaGaikwad <sgai0002@student.monash.edu>
2020-08-15 12:58:17 +1000

    Basic template added

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage(){
+
+        $movies = [
+            'Inception',
+            'Blood Diamond',
+            'Titanic'
+        ];
+       return $this -> render('home/homepage.html.twig',[
+           'welcome' => ucwords("Welcome Sana"),
+           'movies' => $movies,
+        ]);
+
+    }
+}
