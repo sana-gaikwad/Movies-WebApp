1645f7620775fd522f70512f942954724fcd44c2
SanaGaikwad <sgai0002@student.monash.edu>
2020-08-16 11:26:07 +1000

    Home Page updated

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/",name="app_homepage")
      */
     public function homepage(){
 
